cc6a411149d625f0f7e225636f809538c6b6bec7
Belu20 <isanseptian20@gmail.com>
2025-11-25 20:49:19 +0700

    Looping

--- pengaturProgram/looping.js
+++ pengaturProgram/looping.js
@@ -1,1 +1,41 @@
-//FOR
+//FOR IN
+const myOb = {
+  nama: "Belu",
+  namaBelakang: "septian",
+  namatengah: "keren",
+};
+
+for (const property in myOb) {
+  console.log(`${property} bernilai ${myOb[property]}`);
+}
+
+//for loop
+
+for (let i = 0; i < 5; i++) {
+  console.log(`nilai ${i} adalah ${i}`);
+}
+
+//for of
+
+const names = ["ikan", "ayam", "telor", "tahu"];
+for (const item of names) {
+  console.log(item);
+}
+
+//while
+/*
+While statement akan mengeksekusi statement ketika kondisinya bernilai truthy
+while (condition){
+    statement
+}
+*/
+
+let i = 0;
+while (i < 5) {
+  console.log(`Angka ke ${i} adalah ${i}`);
+  i++;
+}
+
+/* DO WHILE
+
+*/
