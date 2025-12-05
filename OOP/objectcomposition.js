2fcb87b175466eae8688cfc01824b934f716c319
Belu20 <isanseptian20@gmail.com>
2025-12-05 20:55:20 +0700

    object compotiotion

--- OOP/objectcomposition.js
+++ OOP/objectcomposition.js
@@ -1,4 +1,4 @@
-class character {
+class Character {
   constructor(name, health, position) {
     this.name = name;
     this.health = health;
@@ -9,3 +9,60 @@ class character {
     console.log(`${this.name} moves to another position`);
   }
 }
+
+function canAttack(character) {
+  return {
+    attack: () => {
+      console.log(`${character.name} attack with a weapon`);
+    },
+  };
+}
+
+function canDefend(character) {
+  return {
+    defend: () => {
+      console.log(`${character.name}  defend with shields`);
+    },
+  };
+}
+
+function canHeal(character) {
+  return {
+    heal: () => {
+      console.log(`${character.name} casts a spell`);
+    },
+  };
+}
+
+//Di JavaScript, kita dapat mengomposisikan objek secara mudah dengan menggunakan method Object.assign(). Object.assign() adalah method statis untuk menyalin semua properti dari satu atau lebih object ke objek target.
+
+function createMonster(name) {
+  const characte = new Character(name, 10, 0);
+  return Object.assign(characte, canAttack(characte));
+}
+
+function createGuardian(name) {
+  const characte = new Character(name, 10, 0);
+  return Object.assign(characte, canDefend(characte));
+}
+
+function createHealer(name) {
+  const characte = new Character(name, 10, 0);
+  return Object.assign(characte, canHeal(characte));
+}
+
+function createWarrior(name) {
+  const characte = new Character(name, 10, 0);
+  return Object.assign(
+    characte,
+    canAttack(characte),
+    canHeal(characte),
+    canDefend(characte)
+  );
+}
+
+const monster = new createWarrior("Ifrit");
+monster.canMove();
+monster.defend();
+monster.attack();
+monster.heal();
