068523988534b77105768d9294f85c0758133661
Belu20 <isanseptian20@gmail.com>
2025-12-16 20:04:31 +0700

    rekursi

---
+++ functionalPrograming/rekursi.js
@@ -0,0 +1,23 @@
+function generateArray(n) {
+  const result = [];
+  for (let counter = 0; counter <= n; counter += 1) {
+    result.push(counter);
+  }
+  return result;
+}
+
+console.log(generateArray(5));
+
+//Dalam FP tidak ada memperbolehkan perubahan data sehingga kita tidak bisa mengubah nilai dari variabel counter
+
+//sehingga di rekursi menjadi seperti ini
+
+function generateArray1(n1) {
+  if (n1 < 0) {
+    return [];
+  }
+
+  return [...generateArray1(n1 - 1), n1];
+}
+
+console.log(generateArray1(5));
