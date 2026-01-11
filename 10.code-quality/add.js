62cd87c874467f2f072c10851ca3c10000b64453
Belu20 <isanseptian20@gmail.com>
2026-01-11 20:18:11 +0700

    flow

---
+++ 10.code-quality/add.js
@@ -0,0 +1,6 @@
+function add(numA: number, numB: number): number {
+  return numA + numB;
+}
+
+const result = add(3, 4);
+console.log("Hasil:", result);
---
+++ 10.code-quality/output.js
@@ -0,0 +1,6 @@
+function add(numA, numB) {
+  return numA + numB;
+}
+
+const result = add(3, 4);
+console.log("Hasil:", result);
